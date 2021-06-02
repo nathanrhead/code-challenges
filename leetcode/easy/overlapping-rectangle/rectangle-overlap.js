aa9adc98a569b7550ca80255816c19c4706511a0
Nathan Rhead Cox <nrc4@hotmail.com>
2021-06-02 12:43:35 -0700

    Add tests.

--- leetcode/easy/overlapping-rectangle/rectangle-overlap.js
+++ leetcode/easy/overlapping-rectangle/rectangle-overlap.js
@@ -1,30 +1,28 @@
 'use strict';
 
 // This is one of Leetcode's provided solutions, which checks the position of each rectangle relative to the other, returning false if there is no overlap (e.g., rec1 is right, left, above, or below rec2) and true otherwise
-const isRectangleOverlap = (rec1, rec2) => {
-  // check if either rectangle is actually a line, since a line cannot have postivie overlap anywhere
-      if (rec1[0] === rec1[2] || rec1[1] === rec1[3] ||
-          rec2[0] === rec2[2] || rec2[1] === rec2[3]) {
-          return false;
-      }
+// const isRectangleOverlap = (rec1, rec2) => {
+//   // check if either rectangle is actually a line, since a line cannot have postivie overlap anywhere
+//       if (rec1[0] === rec1[2] || rec1[1] === rec1[3] ||
+//           rec2[0] === rec2[2] || rec2[1] === rec2[3]) {
+//           return false;
+//       }
 
-      // Return false if rec1 is left, right, below or above rec2; otherwise, true;
-      return !(rec1[2] <= rec2[0] ||   // fully left of
-               rec1[3] <= rec2[1] ||  // fully below
-               rec1[0] >= rec2[2] ||  // fully right of
-               rec1[1] >= rec2[3]);    // fully above
- 
-};
+//       // Return false if rec1 is left, right, below or above rec2; otherwise, true;
+//       return !(rec1[2] <= rec2[0] ||   // fully left of
+//                rec1[3] <= rec2[1] ||  // fully below
+//                rec1[0] >= rec2[2] ||  // fully right of
+//                rec1[1] >= rec2[3]);    // fully above
+// };
 
 /*
-This is solution two from Leetcode, described as follows: "Say the area of the intersection is width * height, where width is the intersection of the rectangles projected onto the x-axis, and height is the same for the y-axis. We want both quantities to be positive.
+This is solution two from Leetcode, based on area and described as follows: "Say the area of the intersection is width * height, where width is the intersection of the rectangles projected onto the x-axis, and height is the same for the y-axis. We want both quantities to be positive.
 
-The width is positive when min(rec1[2], rec2[2]) > max(rec1[0], rec2[0]), that is when the smaller of (the largest x-coordinates) is larger than the larger of (the smallest x-coordinates). The height is similar."
+"The width is positive when min(rec1[2], rec2[2]) > max(rec1[0], rec2[0]), that is when the smaller of the largest x-coordinates is larger than the larger of the smallest x-coordinates. The height is similar."
 */
-
-// const isRectangleOverlap = (rec1, rec2) => {
-//       return (Math.min(rec1[2], rec2[2]) > Math.max(rec1[0], rec2[0]) && // width > 0
-//               Math.min(rec1[3], rec2[3]) > Math.max(rec1[1], rec2[1]));  // height > 0
-//   };
+const isRectangleOverlap = (rec1, rec2) => {
+      return (Math.min(rec1[2], rec2[2]) > Math.max(rec1[0], rec2[0]) && // width > 0
+              Math.min(rec1[3], rec2[3]) > Math.max(rec1[1], rec2[1]));  // height > 0
+  };
 
 module.exports = isRectangleOverlap;
--- leetcode/easy/overlapping-rectangle/rectangle-overlap.test.js
+++ leetcode/easy/overlapping-rectangle/rectangle-overlap.test.js
@@ -1,15 +1,29 @@
 'use strict';
 
-const overlap = require('./rectagle-overlap.js');
+const overlap = require('./rectangle-overlap.js');
 
 describe ('isRectangleOverlap function', () => {
 
-  const rec1 = [0, 1, 2, 0];
-  const rec2 = [0,0, 3, 3];
+  /* Constraints: 
+  rec1[0] <= rec1[2] and rec1[1] <= rec1[3]
+  rec2[0] <= rec2[2] and rec2[1] <= rec2[3]
+  */
 
   it ('returns true if rec1 and rec2 overlaps', () => {
-    expect(overlap(rec1, rec2)).toBe('true');
+    const rec1 = [0, 1, 2, 2];
+    const rec2 = [0, 0, 3, 3];
+    expect(overlap(rec1, rec2)).toBe(true);
   });
 
-  
+  it ('returns false if rec1 and rec2 do not overlap', () => {
+    const rec1 = [0, 0, 2, 2];
+    const rec2 = [3, 3, 4, 4];
+    expect(overlap(rec1, rec2)).toBe(false);
+  });
+
+  it ('returns false when the coordinates of either array create a line instead of a rectangle', () => {
+    const rec1 = [0, 1, 0, 4];
+    const rec2 = [0, 0, 3, 3];
+    expect(overlap(rec1, rec2)).toBe(false);
+  });
 })
