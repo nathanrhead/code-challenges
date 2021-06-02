de1e67b557fb2aadefdcafe43ea9b4656a7026c7
Nathan Rhead Cox <nrc4@hotmail.com>
2021-06-02 14:07:40 -0700

    Complete the bonus challenge for exercise 1, chapter 4 of Elegant JavaScript.

--- elegant-javascript/chapter-4/sum-of-a-range.js
+++ elegant-javascript/chapter-4/sum-of-a-range.js
@@ -4,17 +4,17 @@
 
 // Exercise 1a: Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
 
-function range(start, end) {
+function range(start, end, step = start < end ? 1 : -1) {
   if (start === end) return [start];
 
   let array = [];
 
-  if (start < end) {
-    for (let i = start; i <= end; i++) {
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) {
       array.push(i);
     };
   } else {
-    for (let i = start; i >= end; i--) {
+    for (let i = start; i >= end; i += step) {
       array.push(i);
     }
   };
@@ -31,6 +31,6 @@ function sum(arr) {
   return sum;
 }
 
-// Exercise 1c: As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array.If no step is given, the elements go up by increments of one, corresponding to the old behavior.The function call range(1, 10, 2) should return [1, 3, 5, 7, 9].Make sure it also works with negative step values so that range(5, 2, -1) produces[5, 4, 3, 2].
+// Exercise 1c: As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces[5, 4, 3, 2].
 
 module.exports = { range, sum };
--- elegant-javascript/chapter-4/sum-of-a-range.test.js
+++ elegant-javascript/chapter-4/sum-of-a-range.test.js
@@ -3,7 +3,7 @@
 const { sum } = require('./sum-of-a-range');
 const { range } = require('./sum-of-a-range');
 
-describe ('exercise 1: sum of a range', () => {
+describe ('exercise 1a: create a range of numbers', () => {
 
   it ('creates an array of whole numbers between two given integers in ascending order', () => {
     expect(range(1, 10)).toEqual([1,2,3,4,5,6,7,8,9,10]);
@@ -13,7 +13,20 @@ describe ('exercise 1: sum of a range', () => {
     expect(range(5, 2)).toEqual([5, 4, 3, 2]);
   });
 
-  it ('returns the sume of a range of numbers created by the range and sum functions', () => {
+  it ('returns [start] if start === end', () => {
+    expect(range(4, 4)).toEqual([4]);
+  });
+
+  it ('returns an array of whole numbers incremented by the amount of a third parameter', () => {
+    expect(range(1, 10, 2)).toEqual([1, 3, 5, 7, 9]);
+    expect(range(10, 0, -2)).toEqual([10, 8, 6, 4, 2, 0]);
+  });
+})
+
+describe ('exercise 1b: sum a range of numbers', () => {
+
+  it ('returns the sume of a range of numbers', () => {
     expect(sum(range(1, 10))).toEqual(55);
   });
+
 })
