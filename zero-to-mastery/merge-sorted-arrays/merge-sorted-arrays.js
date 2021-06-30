aed495084c059c48f45c5f802daf000902bad5cf
Nathan Rhead Cox <nrc4@hotmail.com>
2021-06-30 16:44:08 -0700

    Commented the code for the zero-to-mastery challenge to merge two sorted arrays.

--- zero-to-mastery/merge-sorted-arrays/merge-sorted-arrays.js
+++ zero-to-mastery/merge-sorted-arrays/merge-sorted-arrays.js
@@ -1,13 +1,19 @@
 'use strict';
 
 function mergeSortedArrays(arrOne, arrTwo) {
+  // Error checking
   if (!arrOne || !arrTwo || !Array.isArray(arrOne) || !Array.isArray(arrTwo))
     return 'There is nothing to merge.';
+  
+  // Checking for one empty array and returning the second.
   if (arrOne.length < 1) return arrTwo;
   else if (arrTwo.length < 1) return arrOne;
 
-  let result = [];
+  // Variables
+  const result = [];
   let i = 0, j = 0;
+
+  // Loop and logic
   while (i + j < arrOne.length + arrTwo.length) {
     if (arrTwo[j] < arrOne[i] || arrOne[i] === undefined) {
       result.push(arrTwo[j]);
